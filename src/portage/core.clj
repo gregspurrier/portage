886e49231ecc5001731fe867676dda481603131a
Greg Spurrier <greg@sourcematters.org>
2012-07-20 10:58:55 -0700

    Working with portageable async functions.
    But, with the restriction that all forms be lists. Will allow
    unwrapped one-arg fns and one-arg portageable fns in a future
    commit.

--- src/portage/core.clj
+++ src/portage/core.clj
@@ -1,6 +1,20 @@
 (ns portage.core)
 
+(defn portageable?
+  [sym]
+  (boolean (-> sym resolve meta :portageable)))
+
 (defmacro portage->
-  [x f & forms]
-  `(do (~f (-> ~x ~@forms))
-       nil))
+  ([x f]
+     `(do (~f ~x) nil))
+  ([x f form]
+     (if (portageable? (first form))
+       `(do (~(first form) ~f ~x ~@(next form)) nil)
+       `(do (~f (~(first form) ~x ~@(next form))) nil)))
+  ([x f form & more]
+     (if (portageable? (first form))
+       (let [sym (gensym)]
+         `(do (~(first form) (fn [~sym] (portage-> ~sym ~f ~@more))
+               ~x ~@(next form))
+              nil))
+       `(do (portage-> (-> ~x ~form) ~f ~@more) nil))))
--- test/portage/core_test.clj
+++ test/portage/core_test.clj
@@ -3,9 +3,15 @@
         midje.sweet))
 
 (unfinished result-fn one-arg-fn two-arg-fn)
-(fact "non-portagable functions are threaded as with ->"
+
+(defn ^:portageable portage-wrapped-one-arg-fn
+  [cc x]
+  (cc (one-arg-fn x))
+  37)
+
+(fact "non-portageable functions are threaded as with ->"
   (portage-> ..input.. result-fn
-             one-arg-fn
+             (one-arg-fn)
              (one-arg-fn)
              (two-arg-fn ..arg2..))
   => nil
@@ -13,3 +19,34 @@
             (one-arg-fn ..result1..) => ..result2..
             (two-arg-fn ..result2.. ..arg2..) => ..result3..
             (result-fn ..result3..) => ..anything..))
+
+(fact
+  (portage-> ..input.. result-fn
+             (portage-wrapped-one-arg-fn))
+  => nil
+  (provided (one-arg-fn ..input..) => ..result..
+            (result-fn ..result..) => ..anything..))
+
+(fact
+  (portage-> ..input.. result-fn
+             (portage-wrapped-one-arg-fn)
+             (one-arg-fn))
+  => nil
+  (provided (one-arg-fn ..input..) => ..result1..
+            (one-arg-fn ..result1..) => ..result2..
+            (result-fn ..result2..) => ..anything..))
+
+(fact
+  (portage-> ..input.. result-fn
+             (two-arg-fn ..arg1..)
+             (portage-wrapped-one-arg-fn)
+             (one-arg-fn)
+             (portage-wrapped-one-arg-fn)
+             (two-arg-fn ..arg2..))
+  => nil
+  (provided (two-arg-fn ..input.. ..arg1..)   => ..result1..
+            (one-arg-fn ..result1..)          => ..result2..
+            (one-arg-fn ..result2..)          => ..result3..
+            (one-arg-fn ..result3..)          => ..result4..
+            (two-arg-fn ..result4.. ..arg2..) => ..result5..
+            (result-fn ..result5..)           => ..anything..))
