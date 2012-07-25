6678315cb8b06305a8f2939115b40f2395b25bd2
Greg Spurrier <greg@sourcematters.org>
2012-07-25 13:43:22 -0700

    Refactor code to improve readability
    Also added a doc string for -+->

--- src/portage/core.clj
+++ src/portage/core.clj
@@ -1,6 +1,20 @@
 (ns portage.core
   (:use clojure.pprint))
 
+(defn error
+  "Create a Portage error having the value x"
+  [x]
+  {:portage-error x})
+
+(defn error?
+  [x]
+  (and (map? x)
+       (contains? x :portage-error)))
+
+(defn error-value
+  [x]
+  (:portage-error x))
+
 (defn- tagged-with?
   [sym tag]
   (boolean (-> sym resolve meta tag)))
@@ -23,38 +37,49 @@ Forms that are already lists are unchanged."
     form
     (list form)))
 
-(defn error
-  "Create a Portage error having the value x"
-  [x]
-  {:portage-error x})
+(defn- inject-arguments
+  "Splices args into the form as the first argument(s)."
+  [form & args]
+  ;; Preserve metadata like clojure.core/-> does2
+  (with-meta `(~(first form) ~@args ~@(next form)) (meta form)))
 
-(defn error?
-  [x]
-  (and (map? x)
-       (contains? x :portage-error)))
-
-(defn error-value
-  [x]
-  (:portage-error x))
+(defn- continuation-form
+  "Creates a function form representing the continuation of the -+-> expression
+with the remaining-forms."
+  [remaining-forms]
+  (let [sym (gensym)]
+    `(fn [~sym] (-+-> ~sym ~@remaining-forms))))
 
 (defmacro -+->
+  "Threads x through the forms in a manner similar to the -> macro.
+Continuation callbacks are automatically supplied as the first argument
+to functions tagged with the :portageable metadata. This allows normal
+and asynchronous functions--marked as :portageable--to be mixed in
+the same flow.
+
+If a form returns a portage error (created via portage.core/error)
+any following intermediate forms will be skipped unless they are
+tagged with the :accepts-errors metadata. The final form is always
+invoked, regardless of error status. Use portage.core/error? to
+determine whether a value is a portage error and
+portage.core/error-value to retrieve a portage error's wrapped value.
+
+-+-> always returns nil. It is expected that the final form will
+do something useful with the final value."
   ([x form]
      (let [form (promote-to-list form)]
-       `(do (~(first form) ~x ~@(next form))
-            nil)))
+       `(do ~(inject-arguments form x) nil)))
   ([x form & more]
-     (let [form (promote-to-list form)]
-       (if (portageable? (first form))
-         (let [sym (gensym)]
-           (if (accepts-errors? (first form))
-             `(do (~(first form) (fn [~sym] (-+-> ~sym ~@more))
-                     ~x ~@(next form)) nil)
-             `(do (if (error? ~x)
-                    (-+-> ~x ~@more)
-                    (~(first form) (fn [~sym] (-+-> ~sym ~@more))
-                     ~x ~@(next form)))
-                  nil)))
-         (if (accepts-errors? (first form))
+     (let [form (promote-to-list form)
+           op (first form)]
+       (if (portageable? op)
+         (if (accepts-errors? op)
+           `(do ~(inject-arguments form (continuation-form more) x) nil)
+           `(do (if (error? ~x)
+                  (-+-> ~x ~@more)
+                  ~(inject-arguments form (continuation-form more) x))
+                nil))
+         (if (accepts-errors? op)
            `(do (-+-> (-> ~x ~form) ~@more) nil)
            `(do (if (error? ~x)
                   (-+-> ~x ~@more)
